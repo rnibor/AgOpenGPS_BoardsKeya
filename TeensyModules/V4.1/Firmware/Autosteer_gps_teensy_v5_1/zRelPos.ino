a35c5e9fc1f04d5478333aa276fa4850b5f8808d
Brian Tischler <yesgogit@gmail.com>
2023-04-27 10:24:40 -0600

    comment imu code

--- TeensyModules/V4.1/Firmware/Autosteer_gps_teensy_v5_1/zRelPos.ino
+++ TeensyModules/V4.1/Firmware/Autosteer_gps_teensy_v5_1/zRelPos.ino
@@ -46,6 +46,7 @@ void relPosDecode() {
 
     if (carrSoln == 2)
     {
+        if (baseline == 0) baseline += 0.01; 
         rollDual = (asin(relPosD / baseline)) * -RAD_TO_DEG;
         digitalWrite(GPSGREEN_LED, HIGH);   //Turn green GPS LED ON
         imuHandler();             //No IMU so use dual data direct
@@ -55,6 +56,7 @@ void relPosDecode() {
     {
         rollDual *= 0.9;
         digitalWrite(GPSGREEN_LED, blink);  //Flash the green GPS LED
+        dualReadyRelPos = true;   //RelPos ready is true so PAOGI will send when the GGA is also ready
     }
 
     //if (carrSoln == 2)
@@ -75,57 +77,57 @@ void relPosDecode() {
     //}
 }
 
-void imuDualDelta()
-{
-                                        //correctionHeading is IMU heading in radians
-    gpsHeading = heading * DEG_TO_RAD;  //gpsHeading is Dual heading in radians
-
-    //Difference between the IMU heading and the GPS heading
-    gyroDelta = (correctionHeading + imuGPS_Offset) - gpsHeading;
-    if (gyroDelta < 0) gyroDelta += twoPI;
-
-    //calculate delta based on circular data problem 0 to 360 to 0, clamp to +- 2 Pi
-    if (gyroDelta >= -PIBy2 && gyroDelta <= PIBy2) gyroDelta *= -1.0;
-    else
-    {
-        if (gyroDelta > PIBy2) { gyroDelta = twoPI - gyroDelta; }
-        else { gyroDelta = (twoPI + gyroDelta) * -1.0; }
-    }
-    if (gyroDelta > twoPI) gyroDelta -= twoPI;
-    if (gyroDelta < -twoPI) gyroDelta += twoPI;
-
-    //if the gyro and last corrected fix is < 10 degrees, super low pass for gps
-    if (abs(gyroDelta) < 0.18)
-    {
-        //a bit of delta and add to correction to current gyro
-        imuGPS_Offset += (gyroDelta * (0.1));
-        if (imuGPS_Offset > twoPI) imuGPS_Offset -= twoPI;
-        if (imuGPS_Offset < -twoPI) imuGPS_Offset += twoPI;
-    }
-    else
-    {
-        //a bit of delta and add to correction to current gyro
-        imuGPS_Offset += (gyroDelta * (0.2));
-        if (imuGPS_Offset > twoPI) imuGPS_Offset -= twoPI;
-        if (imuGPS_Offset < -twoPI) imuGPS_Offset += twoPI;
-    }
-
-    //So here how we have the difference between the IMU heading and the Dual GPS heading
-    //This "imuGPS_Offset" will be used in imuHandler() when the GGA arrives 
-
-    //Calculate the diffrence between dual and imu roll
-    float imuRoll;
-    imuRoll = (int16_t)roll * 0.1;
-    rollDelta = rollDual - imuRoll;
-    rollDeltaSmooth = (rollDeltaSmooth * 0.7) + (rollDelta * 0.3);
-}
-
-void fuseIMU()
-{     
-    //determine the Corrected heading based on gyro and GPS
-    imuCorrected = correctionHeading + imuGPS_Offset;
-    if (imuCorrected > twoPI) imuCorrected -= twoPI;
-    if (imuCorrected < 0) imuCorrected += twoPI;
-
-    imuCorrected = imuCorrected * RAD_TO_DEG; 
-}
+//void imuDualDelta()
+//{
+//                                        //correctionHeading is IMU heading in radians
+//    gpsHeading = heading * DEG_TO_RAD;  //gpsHeading is Dual heading in radians
+//
+//    //Difference between the IMU heading and the GPS heading
+//    gyroDelta = (correctionHeading + imuGPS_Offset) - gpsHeading;
+//    if (gyroDelta < 0) gyroDelta += twoPI;
+//
+//    //calculate delta based on circular data problem 0 to 360 to 0, clamp to +- 2 Pi
+//    if (gyroDelta >= -PIBy2 && gyroDelta <= PIBy2) gyroDelta *= -1.0;
+//    else
+//    {
+//        if (gyroDelta > PIBy2) { gyroDelta = twoPI - gyroDelta; }
+//        else { gyroDelta = (twoPI + gyroDelta) * -1.0; }
+//    }
+//    if (gyroDelta > twoPI) gyroDelta -= twoPI;
+//    if (gyroDelta < -twoPI) gyroDelta += twoPI;
+//
+//    //if the gyro and last corrected fix is < 10 degrees, super low pass for gps
+//    if (abs(gyroDelta) < 0.18)
+//    {
+//        //a bit of delta and add to correction to current gyro
+//        imuGPS_Offset += (gyroDelta * (0.1));
+//        if (imuGPS_Offset > twoPI) imuGPS_Offset -= twoPI;
+//        if (imuGPS_Offset < -twoPI) imuGPS_Offset += twoPI;
+//    }
+//    else
+//    {
+//        //a bit of delta and add to correction to current gyro
+//        imuGPS_Offset += (gyroDelta * (0.2));
+//        if (imuGPS_Offset > twoPI) imuGPS_Offset -= twoPI;
+//        if (imuGPS_Offset < -twoPI) imuGPS_Offset += twoPI;
+//    }
+//
+//    //So here how we have the difference between the IMU heading and the Dual GPS heading
+//    //This "imuGPS_Offset" will be used in imuHandler() when the GGA arrives 
+//
+//    //Calculate the diffrence between dual and imu roll
+//    float imuRoll;
+//    imuRoll = (int16_t)roll * 0.1;
+//    rollDelta = rollDual - imuRoll;
+//    rollDeltaSmooth = (rollDeltaSmooth * 0.7) + (rollDelta * 0.3);
+//}
+//
+//void fuseIMU()
+//{     
+//    //determine the Corrected heading based on gyro and GPS
+//    imuCorrected = correctionHeading + imuGPS_Offset;
+//    if (imuCorrected > twoPI) imuCorrected -= twoPI;
+//    if (imuCorrected < 0) imuCorrected += twoPI;
+//
+//    imuCorrected = imuCorrected * RAD_TO_DEG; 
+//}
