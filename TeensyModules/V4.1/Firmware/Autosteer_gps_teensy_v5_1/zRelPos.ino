a46ef200a3499edbfe16ae08a7e623ecbfc8e229
Brian Tischler <yesgogit@gmail.com>
2023-04-27 08:28:24 -0600

    Update zRelPos.ino

--- TeensyModules/V4.1/Firmware/Autosteer_gps_teensy_v5_1/zRelPos.ino
+++ TeensyModules/V4.1/Firmware/Autosteer_gps_teensy_v5_1/zRelPos.ino
@@ -5,7 +5,7 @@ void relPosDecode() {
     bool gnssFixOk, diffSoln, relPosValid;
     //bool refPosHeadingValid, relPosNormalized, isMoving, refPosMiss, refObsMiss;
 
-    heading = (int32_t)ackPacket[30] + ((int32_t)ackPacket[31] << 8) 
+    heading = (int32_t)ackPacket[30] + ((int32_t)ackPacket[31] << 8)
         + ((int32_t)ackPacket[32] << 16) + ((int32_t)ackPacket[33] << 24);
     heading *= 0.0001;
 
@@ -16,17 +16,17 @@ void relPosDecode() {
 
     baseline = (int32_t)ackPacket[26] + ((int32_t)ackPacket[27] << 8)
         + ((int32_t)ackPacket[28] << 16) + ((int32_t)ackPacket[29] << 24);
-     double baseHP = (signed char)ackPacket[41];
+    double baseHP = (signed char)ackPacket[41];
     baseHP *= 0.01;
     baseline += baseHP;
-       
+
     relPosD = (int32_t)ackPacket[22] + ((int32_t)ackPacket[23] << 8)
         + ((int32_t)ackPacket[24] << 16) + ((int32_t)ackPacket[25] << 24);
     double relPosHPD = (signed char)ackPacket[40];
     relPosHPD *= 0.01;
     relPosD += relPosHPD;
-    
-    
+
+
     uint32_t flags = ackPacket[66];
 
     //  Serial.println(flags, BIN);
@@ -44,55 +44,35 @@ void relPosDecode() {
     //must be all ok
     if (!gnssFixOk || !diffSoln || !relPosValid) return;
 
-//Base Line Check **********************************************************    
-
-    bool baseLineCheck;
-    static double autoBaseLine = 150;
     if (carrSoln == 2)
     {
-      autoBaseLine = (autoBaseLine * 0.99) + baseline;
-    }
-        
-    if((baseline*100) > (autoBaseLine - baseLineLimit) && (baseline*100) < (autoBaseLine + baseLineLimit))
-    {
-      baseLineCheck = true;
+        rollDual = (asin(relPosD / baseline)) * -RAD_TO_DEG;
+        digitalWrite(GPSGREEN_LED, HIGH);   //Turn green GPS LED ON
+        imuHandler();             //No IMU so use dual data direct
+        dualReadyRelPos = true;   //RelPos ready is true so PAOGI will send when the GGA is also ready
     }
     else
     {
-      baseLineCheck = false;
+        rollDual *= 0.9;
+        digitalWrite(GPSGREEN_LED, blink);  //Flash the green GPS LED
     }
-//**************************************************************************
-   double p = sqrt((baseline * baseline) - (relPosD * relPosD));
-     rollDual = (atan(relPosD / baseline)) * -RAD_TO_DEG;
-   // Serial.println(baseline);
-   //if (carrSoln == 2 && baseLineCheck)
-   //{
-   //  double rawRoll = (atan(relPosD / p)) * -RAD_TO_DEG;
-   // rollDual = (rollDual * 0.8) + (rawRoll * 0.2);
-     digitalWrite(GPSGREEN_LED, HIGH);   //Turn green GPS LED ON
-   // }
-   // else
-   //  {
-   //    rollDual *= 0.9;
-       digitalWrite(GPSGREEN_LED, blink);  //Flash the green GPS LED
-   // }
 
-    if (carrSoln == 2)
-    {
-      if (useBNO08x || useCMPS)
-      {
-        if (baseLineCheck)
-        {
-          imuDualDelta();         //Find the error between latest IMU reading and this dual message
-        }
-        dualReadyRelPos = false;  //RelPos ready is false because we just saved the error for running from the IMU
-      }
-      else
-      {
-        imuHandler();             //No IMU so use dual data direct
-        dualReadyRelPos = true;   //RelPos ready is true so PAOGI will send when the GGA is also ready
-      }
-    }  
+    //if (carrSoln == 2)
+    //{
+    //    if (useBNO08x || useCMPS)
+    //    {
+    //        if (baseLineCheck)
+    //        {
+    //            imuDualDelta();         //Find the error between latest IMU reading and this dual message
+    //        }
+    //        dualReadyRelPos = false;  //RelPos ready is false because we just saved the error for running from the IMU
+    //    }
+    //    else
+    //    {
+    //        imuHandler();             //No IMU so use dual data direct
+    //        dualReadyRelPos = true;   //RelPos ready is true so PAOGI will send when the GGA is also ready
+    //    }
+    //}
 }
 
 void imuDualDelta()
